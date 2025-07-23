f4bd200c3c5da86de2cca11e395bba96aed6f58b
Tamieem Jaffary <tamieem.jaffary@1stdibs.com>
2025-07-23 12:54:30 -0400

    update sunni metadata

--- chunksets/sunni_book_cleaner.py
+++ chunksets/sunni_book_cleaner.py
@@ -2,7 +2,6 @@ import os
 import re
 import pandas as pd
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from text_prepocesser import compress_text
 
 
 
@@ -28,6 +27,8 @@ def process_sunni_csv(csv_path, collection_name, sect="sunni"):
         chapter_raw = row.get("in_book_reference", "")
         grade_en = str(row.get("english_grade", "")).strip() or ""
         grade_ar = str(row.get("arabic_grade", "")).strip() or ""
+        reference = row.get("reference", "").strip() or ""
+        hadith_url = row.get("hadith_url", "").strip() or ""
 
         # Parse book number and title
         match = re.match(r"\s*(\d+)[\.:\-\)]?\s*(.+)?", book_raw)
@@ -43,13 +44,10 @@ def process_sunni_csv(csv_path, collection_name, sect="sunni"):
             book_number = match.group(1)
             hadith_no = match.group(2)
 
-            # Clean texts
+         # Clean texts
         text_en_full = row["english"]
         text_ar_full = row["arabic"]
 
-        # Compress full text
-        compressed_en = compress_text(text_en_full)
-        compressed_ar = compress_text(text_ar_full)
 
         # Determine hadith ID
         hadith_id = f"{sect}_{collection_name}_{book_number}_{hadith_no}"
@@ -69,13 +67,16 @@ def process_sunni_csv(csv_path, collection_name, sect="sunni"):
                 "book_number": book_number,
                 "book_title": book_title,
                 "chapter_title": "",
+                "chapter_number": hadith_no,
                 "hadith_no": hadith_no,
                 "lang": "en",
                 "chunk_id": f"{collection_name}_{chunk_counter}",
                 "grade_en": grade_en,
                 "grade_ar": grade_ar,
-                "text_en": compressed_en,
-                "text_ar": compressed_ar,
+                "text_en": text_ar_full,
+                "text_ar": text_ar_full,
+                "reference": reference,
+                "hadith_url": hadith_url,
                 "hadith_id": hadith_id
             }
 
@@ -94,7 +95,7 @@ if __name__ == "__main__":
     sahih_bukhari = process_sunni_csv("../datasets/Sahih_al-Bukhari_all_books.csv", "sahih-bukhari")
     print(f"Processed {len(sahih_bukhari)} Sahih Bukhari chunks.")
     tirmidhi = process_sunni_csv("../datasets/Jami`_at-Tirmidhi_all_books.csv", "tirmidhi")
-    print(f"Processed {len(sahih_muslim)} Tirmdhi chunks.")
+    print(f"Processed {len(tirmidhi)} Tirmdhi chunks.")
     abi_dawud = process_sunni_csv("../datasets/Sunan_Abi_Dawud_all_books.csv", "abu-dawood")
     print(f"Processed {len(abi_dawud)} Abi Dawud chunks.")
     an_nasai = process_sunni_csv("../datasets/Sunan_an-Nasa'i_all_books.csv", "an-nasai")
